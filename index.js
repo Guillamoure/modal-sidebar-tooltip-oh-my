0124c9bfef69b4bd5a1012af14da3f7a8ad5d175
Guillamoure <jhilscheriv@gmail.com>
2020-04-07 14:31:20 -0400

    set up html, css, and js files

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  const modal = document.getElementById('modal')
+  const sidebar = document.getElementById('sidebar')
+  const tooltip = document.getElementById('tooltip')
+  
+})
